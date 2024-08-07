2dcedbd5ef045743b0854940462ac4f5cf007624
itay601 <itay601@LAPTOP-UI69H6FF>
2024-08-07 14:59:45 +0300

    A.3 - adding json EXM for testing

--- src/index.js
+++ src/index.js
@@ -20,3 +20,21 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
+
+
+nodeExmple = {
+  "title": "My Presentation",
+  "authors": ["Author One", "Author Two"],
+  "dateOfPublishment": "2023-08-01T00:00:00Z",
+  "slides": [
+    {
+      "title": "Slide 1",
+      "content": "Content of Slide 1"
+    },
+    {
+      "title": "Slide 2",
+      "content": "Content of Slide 2"
+    }
+  ]
+}
